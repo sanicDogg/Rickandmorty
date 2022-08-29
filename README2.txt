c4b20dd18d63812bc8a7d160a48335cac1d92776
Alexander <boradach200@mail.ru>
2022-08-30 01:58:48 +0300

    Создана общая структура проекта

--- README2.txt
+++
@@ -1,1 +0,0 @@
-https://github.com/sanicDogg/Rickandmorty
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export * from './Character';
+export * from './Favorites';
+export * from './Home';
+export * from './Signin';
+export * from './Signup';
